46aa65c4df407d49ffc39ea8515d1e624763711a
aveekdeveloper <aveekdeveloper@gmail.com>
2014-05-27 22:56:46 +0530

    corrected rendering

--- api/playground/render_playground.php
+++ api/playground/render_playground.php
@@ -6,21 +6,21 @@ function render_playground($venue)
 
 	<div class="flat_box_body_div ">
 		<h3>
-		<span><font style="text-transform: uppercase;"><b>$venue.Playground_name</b></font> - $venue.Playground_locality</span>
+		<span><font style="text-transform: uppercase;"><b><?php echo $venue.Playground_name ?></b></font> - <?php echo $venue.Playground_locality ?></span>
 		</h3>
-		<span class="soft">$venue.Playground_sports_type | uppercase</span>
+		<span class="soft"><?php echo $venue.Playground_sports_type ?></span>
 		<br/>
 		<br/>
-		Rate : <span class="soft">$venue.Playground_rate per time slot of 1 hour</span>
+		Rate : <span class="soft"><?php echo $venue.Playground_rate ?> per time slot of 1 hour</span>
 		<br/>
-		Address : <span class="soft">$venue.Playground_address</span>
+		Address : <span class="soft"><?php echo $venue.Playground_address ?></span>
 		<br/>
-		About : <span class="soft">$venue.Playground_about</span>
+		About : <span class="soft"><?php echo $venue.Playground_about ?></span>
 		<br/>
-		Contact : <span class="soft">$venue.Playground_number</span>
+		Contact : <span class="soft"><?php echo $venue.Playground_number ?></span>
 		<br/>
 		<br/>
-		<a target="_blank" href="$venue.Playground_google_map">Get directions to this place</a>
+		<a target="_blank" href=<?php echo $venue.Playground_google_map ?>>Get directions to this place</a>
 	</div>
 
 <?php
